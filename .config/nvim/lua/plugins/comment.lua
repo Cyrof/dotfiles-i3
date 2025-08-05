8df9d046d40fd89006f4a56904e0510e4158b81b
keith <keithneo00@gmail.com>
2025-08-06 01:11:07 +0800

    fix(plugins/comment): Fix bug when commenting using visual mode sometimes it does not work well in visual mode

--- .config/nvim/lua/plugins/comment.lua
+++ .config/nvim/lua/plugins/comment.lua
@@ -3,23 +3,27 @@ return {
         "numToStr/Comment.nvim",
         event = { "BufReadPre", "BufNewFile" },
         config = function()
-            require("Comment").setup({
+            local comment = require("Comment")
+
+            comment.setup({
                 toggler = { line = "gcc", block = "gbc" },
                 opleader = { line = "gc", block = "gb" },
             })
 
-            -- keymap for line comment
+            local api = require("Comment.api")
+
+            -- Normal mode mappings
             vim.api.nvim_set_keymap("n", "<leader>/l", "<cmd>lua require('Comment.api').toggle.linewise.current()<CR>",
                 { noremap = true, silent = true })
-            vim.api.nvim_set_keymap("x", "<leader>/l",
-                "<cmd>lua require('Comment.api').toggle.linewise(vim.fn.visualmode())<CR>",
-                { noremap = true, silent = true })
-
-            -- keymap for block comment
             vim.api.nvim_set_keymap("n", "<leader>/b", "<cmd>lua require('Comment.api').toggle.blockwise.current()<CR>",
                 { noremap = true, silent = true })
+
+            -- Visual mode mappings (use :vmap and lua function to get correct range)
+            vim.api.nvim_set_keymap("x", "<leader>/l",
+                ":lua require('Comment.api').toggle.linewise(vim.fn.visualmode())<CR>",
+                { noremap = true, silent = true })
             vim.api.nvim_set_keymap("x", "<leader>/b",
-                "<cmd>lua require('Comment.api').toggle.blockwise(vim.fn.visualmode())<CR>",
+                ":lua require('Comment.api').toggle.blockwise(vim.fn.visualmode())<CR>",
                 { noremap = true, silent = true })
         end,
     },
